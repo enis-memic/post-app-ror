16be8be63bae0541026faeedac1d7a0f1c732b72
Enis <enis.mema@gmail.com>
2023-08-17 17:43:33 +0200

    Modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  
+  def recent_posts
+    posts.order(created_at: :desc)
+  end
 end
