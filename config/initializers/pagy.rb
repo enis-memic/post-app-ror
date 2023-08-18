92516ba934268ed9ec0911fe6f73d1d4ac0b9214
Enis <enis.mema@gmail.com>
2023-08-18 16:50:17 +0200

    Update pagy.rb

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,4 +3,5 @@ Pagy::DEFAULT[:items] = 10        # items per page
 Pagy::DEFAULT[:size]  = [1,4,4,1] # nav bar links
 # Better user experience handled automatically
 require 'pagy/extras/overflow'
+require 'pagy/extras/bootstrap'
 Pagy::DEFAULT[:overflow] = :last_page
