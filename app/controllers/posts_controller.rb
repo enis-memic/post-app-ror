2d075e583b7440d0084874ce40dc1fbb97494004
Enis <enis.mema@gmail.com>
2023-08-18 16:51:34 +0200

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-    
+  before_action :authenticate_user!, except: [:index, :show]
 
 
     def index
-        @pagy, @posts = pagy(Post.all, items: 10)
+        @pagy, @posts = pagy(Post.all, items: 2)
     end
 
     def show
@@ -12,7 +12,11 @@ class PostsController < ApplicationController
       end
     
       def new
-        @post = Post.new
+        if user_signed_in?
+          @post = Post.new
+        else
+          redirect_to new_user_session_path, alert: "You need to sign in to create a new post."
+        end
       end
     
       def create
@@ -24,9 +28,24 @@ class PostsController < ApplicationController
           render :new
         end
       end
+
+      def edit
+        @post = Post.find(params[:id])
+      end
+    
+      def update
+        @post = Post.find(params[:id])
+      
+        if @post.update(post_params)
+          redirect_to post_path(@post), notice: 'Post updated successfully.'
+        else
+          render :edit
+        end
+      end
     
       def destroy
         @post = Post.find(params[:id])
+        authorize! :destroy, @post
         @post.comments.destroy_all
         if @post.destroy
           redirect_to posts_path(current_user.id)
