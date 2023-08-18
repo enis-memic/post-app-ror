8891d1257d94efb0a16a87ebb16f3ac6c7b222c1
Enis <enis.mema@gmail.com>
2023-08-18 16:53:05 +0200

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     has_many :comments, foreign_key: 'post_id'
+
+    validate :no_comments_present_before_deletion, on: :destroy
+
+  private
+
+  def no_comments_present_before_deletion
+    if comments.any?
+      errors.add(:base, "Cannot delete post with comments.")
+    end
+  end
 end
